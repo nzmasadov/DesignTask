1d905b6d64a04813e0a8c7aba253d9614aae415d
Nazim Asadov <nazimatmu@gmail.com>
2022-03-09 15:33:40 +0400

    Initial Commit

---
+++ NewTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewTask
+//
+//  Created by Nazim Asadov on 09.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
